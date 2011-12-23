eed30e3b3e393cde5012d2d4d76fd709036b8333
James Turner <james.turner.phpninja@gmail.com>
2011-12-23 08:33:17 +0000

    Allowed multiple page limits to be passed in as a paramter to the App

--- src/WeGotTickets/App.php
+++ src/WeGotTickets/App.php
@@ -12,18 +12,21 @@ class App {
     static public function run($argv){
 
         $uri = 'http://www.wegottickets.com/searchresults/page/1/all';
-
+        $limit = 1;
         // parse cmd line args.
         if(count($argv) > 1){
             if(false === parse_url($argv[1])){
                 throw new \RuntimeException("Check that the supplied URI via cmd line is a valid URI.");
             }
             $uri = $argv[1];
+            if(isset($argv[2])){
+                $limit = $argv[2];
+            }
         }
 
         $crawler = new ZendHttpClientAdapter(new \Zend_Http_Client());
         $scraper = new Scraper($crawler, new Parser(), new JSONFormatter(), new Stdout());
-        $scraper->scrape($uri, 1);
+        $scraper->scrape($uri, $limit);
 
     }
 
